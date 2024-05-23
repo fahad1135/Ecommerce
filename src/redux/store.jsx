019286c3c936cb94898945dbabe197542fc1c070
FahadMajeed <fahadmajeed154@gmail.com>
2024-05-23 16:13:39 +0500

    update store file

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import cartSlice from "./cartSlice";
-export const store = configureStore({
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSlice);
+
+const store = configureStore({
   reducer: {
-    cart: cartSlice,
+    cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
